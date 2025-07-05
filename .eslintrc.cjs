a97d17d85cbd1789ec6827b57d55e902cecb1091
tiagobravobr <tiago@bravolideres.com.br>
2025-07-04 21:59:25 -0400

    chore: configura ESLint, Prettier e dependências para padronização do código

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,45 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: './tsconfig.json',
+  },
+  plugins: [
+    'react',
+    'react-hooks',
+    '@typescript-eslint',
+    'prettier',
+    'react-refresh',
+  ],
   extends: [
     'eslint:recommended',
-    '@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
   rules: {
+    'prettier/prettier': 'warn',
+    'no-console': 'warn',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    // Outras regras personalizadas podem ser adicionadas aqui
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
 }
